8716dd707d736f12724f6010cd5c8d42b0175a01
WalterCun <waltercunbustamante@gmail.com>
2025-02-24 23:50:43 -0500

    Add nested serializers for Hotel, Location, City, and Service
    Introduce `ServiceSerializer`, `CitySerializer`, and `LocationSerializer` to handle nested data structures. Update `HotelSerializer` to include location details and filter active services using a custom method field.

--- apps/api/serializers/hotel_serial.py
+++ apps/api/serializers/hotel_serial.py
@@ -1,9 +1,58 @@
+from rest_framework.fields import CharField, SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
-from apps.api.models import Hotel
+from apps.api.models import Hotel, Location, Service
+from apps.core.models import City
+
+
+class ServiceSerializer(ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ['name', 'description', 'is_active']
+
+
+class CitySerializer(ModelSerializer):
+    province = CharField(source='province.name')
+    country = CharField(source='province.country.name')
+
+    class Meta:
+        model = City
+        fields = ['name', 'province', 'country']
+
+
+class LocationSerializer(ModelSerializer):
+    city = CharField(source='city.name')
+    province = CharField(source='city.province.name')
+    country = CharField(source='city.province.country.name')
+
+    class Meta:
+        model = Location
+        fields = [
+            "city",
+            "province",
+            "country",
+            "main_street",
+            "house_number",
+            "intersection",
+            "latitude",
+            "longitude",
+            "map_url",
+            "is_active",
+        ]
 
 
 class HotelSerializer(ModelSerializer):
+    location = LocationSerializer()
+    # services = ServiceSerializer(many=True)
+    services = SerializerMethodField()
+
     class Meta:
         model = Hotel
-        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
+
+    def get_services(self, instance):
+        """
+        Filtrar informacion de servicios activos.
+        """
+        active_services = instance.services.filter(is_active=True)
+        return ServiceSerializer(active_services, many=True).data
