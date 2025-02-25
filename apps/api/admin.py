4ad11c9b2f377e7d2a82930d44ae907168b0da83
WalterCun <waltercunbustamante@gmail.com>
2025-02-24 23:50:05 -0500

    Add admin registrations for additional models
    Registered multiple models in Django admin, including Offer, OfferMetric, PageStatistics, and others for better management and visibility. This enhances the admin interface to support handling these entities directly.

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
 from apps.api.forms.location_form import HotelForm
-from apps.api.models import Hotel, Location, Service
+from apps.api.models import Hotel, Location, Service, Offer, OfferMetric, PageStatistics, OfferTag, ProspectSubject, \
+    ProspectSource, Prospect, SocialNetwork, SocialPlatform, TourCategory, PriceType, TourDestination, TourImage, \
+    TourVideo
 
 
+######################################################################################################################
 @admin.register(Hotel)
 class HotelAdmin(ModelAdmin):
     list_display = ['name', 'location', 'stars', 'is_active']
@@ -24,7 +27,81 @@ class LocationAdmin(ModelAdmin):
     ordering = ("city", "main_street")
 
 
-
 @admin.register(Service)
 class ServiceAdmin(ModelAdmin):
+    pass
+
+
+######################################################################################################################
+@admin.register(OfferMetric)
+class OfferMetricAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(PageStatistics)
+class PageStatisticsAdmin(ModelAdmin):
+    pass
+
+
+######################################################################################################################
+@admin.register(OfferTag)
+class OfferTagAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Offer)
+class OfferAdmin(ModelAdmin):
+    pass
+
+
+######################################################################################################################
+@admin.register(ProspectSubject)
+class ProspectSubjectAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(ProspectSource)
+class ProspectSourceAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Prospect)
+class ProspectAdmin(ModelAdmin):
+    pass
+
+
+######################################################################################################################
+@admin.register(SocialPlatform)
+class SocialPlatformAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(SocialNetwork)
+class SocialNetworkAdmin(ModelAdmin):
+    pass
+
+
+######################################################################################################################
+@admin.register(TourCategory)
+class TourCategoryAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(PriceType)
+class PriceTypeAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(TourDestination)
+class TourDestinationAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(TourImage)
+class TourImageAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(TourVideo)
+class TourVideoAdmin(ModelAdmin):
     pass
