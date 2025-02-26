350e6f1c96437f05c8ad1749ff09491b13108a14
WalterCun <waltercunbustamante@gmail.com>
2025-02-26 00:14:34 -0500

    Add new URL patterns and view imports for API modules
    Extended the API routes to include additional URL patterns for offers, metrics, prospects, tours, social networks, and others. Updated `views/__init__.py` to import the corresponding views, ensuring proper routing for the newly added modules. This change improves the modularity and scalability of the API.

--- apps/api/urls/__init__.py
+++ apps/api/urls/__init__.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('v1/', include('apps.api.urls.hotel_url')),
-    # path('v1/', include(hotel_url)),
+    path('v1/', include('apps.api.urls.all_url')),
+    path('v1/', include('apps.api.urls.metrics_url')),
+    path('v1/', include('apps.api.urls.offer_url')),
+    path('v1/', include('apps.api.urls.prospect_url')),
+    path('v1/', include('apps.api.urls.social_network_url')),
+    path('v1/', include('apps.api.urls.tours_url')),
 ]
---
+++ apps/api/views/__init__.py
@@ -0,0 +1,6 @@
+from .all_view import *
+from .offer_view import *
+from .tours_view import *
+from .metrics_view import *
+from .prospect_view import *
+from .social_network_view import *
